39fd0bee3812837b5a8d01f1719e2b3925756867
szincone <sawyerzincone@gmail.com>
2018-12-06 19:39:14 -0700

    added vscode styles.json for custom title bar, added boilerplate for contact view

---
+++ src/components/contact/Contact.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Contact extends Component {
+  render() {
+    return (
+      <>
+        <placeholer />
+      </>
+    )
+  }
+}
+
+export default Contact
