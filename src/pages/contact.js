b19756556684bd5c73cb709d136ce0f27c21a30c
szincone <sawyerzincone@gmail.com>
2018-12-08 18:09:07 -0700

    basic form bolier plate done

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,7 +5,42 @@ class Contact extends Component {
   render() {
     return (
       <>
-        <form />
+        <form name="contact" method="POST" netlify>
+          <p>
+            <label>
+              Company:
+              <input type="text" name="company" value="" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Name:
+              <input type="text" name="name" value="" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Phone:
+              <input type="text" name="phone" value="" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Email:
+              <input type="text" name="email" value="" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Message: <textarea name="message" />
+            </label>
+          </p>
+          <p>
+            <button type="submit" className="button special">
+              Send
+            </button>
+          </p>
+        </form>
         <Link to="/" className="button special">
           Return Home
         </Link>
