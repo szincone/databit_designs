8974c286574acce859b2dba54139e1eaff54b072
szincone <sawyerzincone@gmail.com>
2018-12-09 16:52:16 -0700

    removed styled-components, updated form styling - weird issue w/ gatsby npm styled-components - almost finished w/ contact form styling

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,50 +1,81 @@
 import React, { Component } from 'react'
 import { Link } from 'gatsby'
 
+const formContainer = {
+  maxWidth: '30rem',
+  width: '100%',
+  background: 'red',
+  display: 'flex',
+  justifyContent: 'center',
+  flexDirection: 'column',
+}
+const centerCol = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  flexDirection: 'column',
+}
+const centerRow = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  flexDirection: 'row',
+}
+const pStyle = {
+  padding: '1rem',
+  marginBottom: '0',
+  paddingBottom: '0',
+}
 class Contact extends Component {
   render() {
     return (
-      <>
-        <form name="contact" method="POST" netlify>
-          <p>
-            <label>
-              Company:
-              <input type="text" name="company" value="" />
-            </label>
-          </p>
-          <p>
-            <label>
-              Name:
-              <input type="text" name="name" value="" />
-            </label>
-          </p>
-          <p>
-            <label>
-              Phone:
-              <input type="text" name="phone" value="" />
-            </label>
-          </p>
-          <p>
-            <label>
-              Email:
-              <input type="text" name="email" value="" />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message: <textarea name="message" />
-            </label>
-          </p>
-          <p>
-            <button type="submit" className="button special">
-              Send
-            </button>
-          </p>
-        </form>
-        <Link to="/" className="button special">
-          Return Home
-        </Link>
-      </>
+      <div style={centerCol}>
+        <div style={formContainer}>
+          <form name="contact" method="POST" netlify="true">
+            <div>
+              <p style={pStyle}>
+                <label style={{ maxWidth: '47%' }}>
+                  Company:
+                  <input type="text" name="company" value="" />
+                </label>
+              </p>
+              <p style={pStyle}>
+                <label>
+                  Name:
+                  <input type="text" name="name" value="" />
+                </label>
+              </p>
+            </div>
+            <div style={centerRow}>
+              <p style={pStyle}>
+                <label>
+                  Phone:
+                  <input type="text" name="phone" value="" />
+                </label>
+              </p>
+              <p style={pStyle}>
+                <label>
+                  Email:
+                  <input type="text" name="email" value="" />
+                </label>
+              </p>
+            </div>
+            <p style={pStyle}>
+              <label>
+                Message: <textarea name="message" />
+              </label>
+            </p>
+            <p style={centerCol}>
+              <button type="submit" className="button special">
+                Send
+              </button>
+            </p>
+          </form>
+          <Link to="/" className="button special">
+            Return Home
+          </Link>
+        </div>
+      </div>
     )
   }
 }
